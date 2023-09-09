bb202ff18b85eae577ba6ca22ab1b95e6822d6ac
kdds598 <132571304+kdds598@users.noreply.github.com>
2023-09-09 19:48:36 +0530

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,23 @@
+console.log("i love pizza");
+window.alert("hello this is an alert");
+
+//var by let 
+
+let age;
+age =19;
+let fname="rishi"
+let student =true;
+console.log("hello",fname);
+console.log("you are ",age,"years old");
+console.log("enrolled ",student);
+
+
+// changing or using items on html file by using their id
+
+document.getElementById("p1").innerHTML="hello" + fname;
+document.getElementById("p2").innerHTML="you are "+age+"years old";
+document.getElementById("p3").innerHTML="enrolled "+student;
+
+//operators arithmetic operations
+
+//user input
